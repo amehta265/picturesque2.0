9d07569238e36d94c86a67179a2cd19ccc95c179
Ankit Mehta <ankitmehta265@gmail.com>
2021-11-30 15:42:11 -0500

    Completed Stats page with all nutriotional info + pie chart macro-nutrients

--- screens/StatsScreen.tsx
+++ screens/StatsScreen.tsx
@@ -4,6 +4,10 @@ import { View, Text} from '../components/Themed';
 import Markdown from 'react-native-markdown-display';
 import {AppConfig} from  "../config";
 import {getColor} from "../components/Themed";
+import { PieChart } from 'react-native-svg-charts';
+//import {Card} from 'react-native-elements';
+//import {SafeAreaView, ScrollView, Image, Alert, ImageBackground, FlatList} from "react-native"
+import {Toast, Button, Container, Icon, Content, CardItem, Card, List, ListItem, Header} from 'native-base';
 const axios = require('axios');
 var nutritionKeys = require('../nutritionApiKey');
 var nutritionID = nutritionKeys.apiID;
@@ -13,16 +17,17 @@ interface IProps {
 }
 
 interface IState {
-  calories: Number,
-  cholesterol: Number,
-  dietaryFiber: Number, 
-  protein: Number,
-  saturatedFat: Number,
-  sugars: Number,
-  totalCarbs: Number,
-  totalFat: Number
-  servingQty: Number,
-  servingUnit: String
+  calories: number,
+  cholesterol: number,
+  dietaryFiber: number, 
+  protein: number,
+  saturatedFat: number,
+  sugars: number,
+  totalCarbs: number,
+  totalFat: number
+  servingQty: number,
+  servingUnit: String,
+  name: String
 }
 
 
@@ -39,7 +44,8 @@ export default class StatsScreen extends Component<IProps, IState> {
       totalCarbs: 0,
       totalFat: 0,
       servingQty: 0,
-      servingUnit: ""
+      servingUnit: "",
+      name: ""
     }
   }
 
@@ -49,6 +55,7 @@ export default class StatsScreen extends Component<IProps, IState> {
 
   getInformation = () => {
     let data = "chicken noodle soup";
+    this.setState({name: data});
     const headers = {
         'content-type': 'application/json',
         'x-app-id': nutritionID.toString(),
@@ -58,17 +65,18 @@ export default class StatsScreen extends Component<IProps, IState> {
     axios.post('https://trackapi.nutritionix.com/v2/natural/nutrients', {query: data}, {headers: headers})
     .then((response: any) => {
         const foods = response.data.foods[0]
-        const calories = foods.nf_calories;
-        const cholesterol = foods.nf_cholesterol;
-        const dietaryFiber = foods.nf_dietary_fiber;
-        const protein = foods.nf_protein;
-        const saturatedFat = foods.nf_saturated_fat;
+        const cal = foods.nf_calories;
+        const chol = foods.nf_cholesterol;
+        const dietFiber = foods.nf_dietary_fiber;
+        const prot = foods.nf_protein;
+        const saturatedF = foods.nf_saturated_fat;
         const sugars = foods.nf_sugars;
-        const totalCarbs = foods.nf_total_carbohyrdate;
-        const totalFat = foods.nf_total_fat;
-        const servingQty = foods.serving_qty;
-        const servingUnit = foods.serving_unit;
-        this.setState({ calories, cholesterol, dietaryFiber, protein, saturatedFat, sugars, totalCarbs, totalFat, servingQty, servingUnit })
+        const TCarbs = foods.nf_total_carbohydrate;
+        const TFat = foods.nf_total_fat;
+        const servQty = foods.serving_qty;
+        const servUnit = foods.serving_unit;
+        this.setState({ calories: cal, cholesterol: chol , dietaryFiber: dietFiber, protein: prot, 
+          saturatedFat: saturatedF, sugars: sugars, totalCarbs: TCarbs, totalFat: TFat, servingQty: servQty, servingUnit:servUnit })
     }).catch(function(error: any) {
         console.error(error);
     })
@@ -79,19 +87,112 @@ export default class StatsScreen extends Component<IProps, IState> {
   }
 
   render() {
-    const {calories,cholesterol,dietaryFiber,protein,saturatedFat,sugars,totalCarbs,totalFat ,servingQty,servingUnit} = this.state
+    const {calories,cholesterol,dietaryFiber,protein,saturatedFat,sugars,totalCarbs,totalFat,servingQty,servingUnit,name} = this.state
     return (
-      <View style={styles.container}>
-        <View style={{}}>
-          <Text h2 >Nutritional Information</Text>
+      <Container>
+        <Content>
+          <View>
+        <Card
+          style={{alignItems: 'center', padding: 10}}
+        >
+        <CardItem
+        header
+        style={{alignSelf: 'center'}}
+        >
+            <Text
+              style={{fontWeight: 'bold', fontSize: 20, color:"black"}}> {name.toUpperCase()}</Text>
+            </CardItem>
+            <CardItem
+            style={{alignSelf: 'center'}}>
+              <Text
+                style={{fontWeight: 'bold', fontSize: 15, color:"black"}}>{calories} calories </Text>
+            </CardItem>
+            <CardItem
+            style={{alignSelf: 'center'}}>
+            <Text style={{fontWeight: 'bold', fontSize: 15, color:"black"}}>  Serving: {servingQty} {servingUnit} </Text>
+            </CardItem>
+            <CardItem
+              style={{alignSelf: 'center'}}>
+            </CardItem>
+            <CardItem>
+          <PieChart
+          style={{ height: 250, width: 250 }}
+          outerRadius={'70%'}
+          innerRadius={10}
+          data={[
+            {
+              key: 1,
+              value: protein,
+              svg: { fill: '#bc5090' },
+              arc: { cornerRadius: 5 }
+            },
+            {
+              key: 2,
+              value: totalCarbs,
+              svg: { fill: '#ff6361' },
+              arc: { cornerRadius: 5 }
+            },
+            {
+              key: 3,
+              value: totalFat,
+              svg: { fill: '#ffa600' },
+              arc: { cornerRadius: 5 }
+            }
+          ]}
+          />
+            </CardItem>
+            <CardItem
+              style={{flex: 1}}>
+              <Button
+                style={{backgroundColor: '#bc5090'}}>
+                <Text style={{color: 'black', fontWeight: 'bold'}}>Protein {protein.toFixed(2)}g</Text>
+              </Button>
+              <Button
+                style={{backgroundColor: '#ff6361'}}>
+                <Text style={{color: 'black', fontWeight: 'bold'}}>Carbs {totalCarbs.toFixed(2)}g</Text>
+              </Button>
+              <Button
+                style={{backgroundColor: '#ffa600'}}>
+                <Text style={{color: 'black', fontWeight: 'bold'}}>Fat {totalFat.toFixed(2)}g</Text>
+              </Button>
+            </CardItem>
+        </Card>
         </View>
-        <View>
-              <Text style={{alignSelf: 'center'}}>Click the food that best matches your picture</Text>
-              <Text> Calories: </Text> 
-              <Text> {calories}, {protein}</Text>
-        </View>
-    </View>
-
+        <Card>
+            <List>
+            <ListItem itemDivider>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>
+                {servingQty} {servingUnit} {name.toUpperCase()}
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Calories: {calories}kcal</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Total Fat: {totalFat}g</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Carbs: {totalCarbs}g</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Sugar: {sugars}g</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Cholesterol: {cholesterol}g</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Protein: {protein}g</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Dietary Fiber: {dietaryFiber}g</Text>
+            </ListItem>
+            <ListItem>
+              <Text style={{color: 'black', fontWeight: 'bold'}}>Saturated Fat: {saturatedFat}g</Text>
+            </ListItem>
+          </List>
+        </Card>
+        </Content>
+      </Container>
     )
   }
 }
@@ -101,5 +202,3 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10
   }
 });
-
-
