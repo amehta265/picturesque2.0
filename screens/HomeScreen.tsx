5d8ca919b8d3953ea5c02db1ac659af5e4c4dd4f
maya280 <mayaelee28@gmail.com>
2021-12-04 18:21:11 -0500

    Added images and edited homescreen UI
    Homescreen UI updated and necessary images added.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect} from "react"
-import {SafeAreaView, ScrollView, Image, Text, Alert, View, ImageBackground, FlatList} from "react-native"
+import {SafeAreaView, ScrollView, Image, Text, Alert, View, ImageBackground, FlatList, TouchableOpacity} from "react-native"
 import {StyleSheet} from "react-native";
+import { white } from "react-native-paper/lib/typescript/styles/colors";
+import { NavigationContainer } from '@react-navigation/native';
 //import style from "./style";
 
-const Dashboard = () => {
+const Dashboard = ({navigation}: {navigation: any}) => {
   const [recentFoods, setRecentFoods] = React.useState<string[]>([])
 
   useEffect(() => {
@@ -12,12 +14,21 @@ const Dashboard = () => {
 
   return (
     <SafeAreaView style={style.container}>
-      <ScrollView>
-        <Text style={style.titleText}> ADD RECENT FOODS</Text>
+      <ImageBackground source={require('../assets/images/background.png')} resizeMode="cover" style={{flex:1, marginTop: 50}}>
+        <View style={{alignItems: 'center', marginTop: 60}}>
+          <Image style={{width: 400, height: 300}} source={require('../assets/images/logo2.png')}/>
+        </View>
+        <View style={{alignItems: 'center', marginTop: 50}}>  
+          <TouchableOpacity style={style.cameraButton} onPress={() => navigation.navigate('Camera')}>
+            <Image style={{width: 200, height: 150}} source={require('../assets/images/takePicText.png')}/>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+        {/* <Text style={style.titleText}> ADD RECENT FOODS</Text>
         <View style={{alignItems: 'center', paddingVertical: 5}}>
           <View style={{width:'80%'}}>
             <ImageBackground resizeMode="cover" style={{justifyContent: 'center', alignItems: 'center', overflow: 'hidden', borderWidth: 3, borderColor: 'black', width: '100%', height: 100, borderRadius: 10}} source={require('../assets/images/apple.jpg')}>
-              <Image source={require('../assets/images/addAppleButton.png')}/>
+              <Image source={require('../assets/images/logo.png')}/>
             </ImageBackground>
           </View>
         </View>
@@ -39,8 +50,7 @@ const Dashboard = () => {
         </View>
         <View style={{alignItems: 'center', paddingVertical: 5}}>
           <Image style={{borderWidth: 3, borderColor: 'black', width: '80%', height: 100, borderRadius: 10}} source={require('../assets/images/pasta.jpg')} /> 
-        </View>
-      </ScrollView>
+        </View> */}
     </SafeAreaView>
   )
 }
@@ -51,7 +61,10 @@ const style = StyleSheet.create({
     backgroundColor: "#fff",
   },
   baseText: {
-    fontFamily: "Comfortaa"
+    color: 'black', 
+    fontSize: 30,
+    padding: 10,
+    fontFamily: 'Comfortaa'
   },
   titleText: {
     fontSize: 36,
@@ -59,6 +72,14 @@ const style = StyleSheet.create({
   },
   button: {
     position: 'absolute', 
+  },
+  cameraButton: {
+    width: 300,
+    height: 200,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
